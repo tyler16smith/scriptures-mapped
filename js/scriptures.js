56d20e51ca0c6f26be9f10b29a7b94cee8877555
Tyler Smith <tylersmithlf@gmail.com>
2022-01-31 11:03:26 -0700

    Adds hash change

--- js/scriptures.js
+++ js/scriptures.js
@@ -10,8 +10,9 @@
     browser
 */
 /*property
-    books, forEach, init, maxBookId, minBookId, onerror, onload, open, parse,
-    push, response, send, status
+    books, classKey, content, forEach, hash, href, id, init, location, log,
+    maxBookId, minBookId, onHashChanged, onerror, onload, open, parse, push,
+    response, send, status
 */
 
 const Scriptures = (function () {
@@ -20,6 +21,15 @@ const Scriptures = (function () {
     /*-------------------------------------------------------------------
      *                      CONSTANTS
      */
+    const BOTTOM_PADDING = "<br /><br />";
+    const CLASS_BOOKS = "books";
+    const CLASS_VOLUME = "volume";
+    const DIV_SCRIPTURES_NAVIGATOR = "scripnav";
+    const DIV_SCRIPTURES = "scriptures";
+    const REQUEST_GET = "GET";
+    const REQUEST_STATUS_OK = 200;
+    const REQUEST_STATUS_ERROR = 400;
+    const TAG_HEADERS = "h5";
     const URL_BASE = "https://scriptures.byu.edu/";
     const URL_BOOKS = `${URL_BASE}mapscrip/model/books.php`;
     const URL_VOLUMES = `${URL_BASE}mapscrip/model/volumes.php`;
@@ -35,8 +45,18 @@ const Scriptures = (function () {
      */
     let ajax;
     let cacheBooks;
+    let htmlAnchor;
+    let htmlDiv;
+    let htmlElement;
+    let htmlLink;
+    let htmlHashLink;
     let init;
     let testGeoplaces;
+    let onHashChanged;
+    let navigateHome;
+    let navigateBook;
+    let navigateChapter;
+    let bookChapterValid;
 
     /*-------------------------------------------------------------------
      *                      PRIVATE METHODS
@@ -44,10 +64,10 @@ const Scriptures = (function () {
     ajax = function (url, successCallback, failureCallback) {
         let request = new XMLHttpRequest();
 
-        request.open("GET", url, true);
+        request.open(REQUEST_GET, url, true);
 
         request.onload = function () {
-            if (request.status >= 200 && request.status < 400) {
+            if (request.status >= REQUEST_STATUS_OK && request.status < REQUEST_STATUS_ERROR) {
                 let data = JSON.parse(request.response);
 
                 if (typeof successCallback === "function") {
@@ -63,6 +83,20 @@ const Scriptures = (function () {
         request.onerror = failureCallback;
         request.send();
     };
+
+    bookChapterValid = function (bookId, chapter) {
+        let book = books[bookId];
+
+        // chapters can be zero (title page of the Book of Mormon) unless the book HAS chapters
+        if (book === undefined || chapter < 0 || chapter > book.numChapters) {
+            return false;
+        }
+        if (chapter === 0 && book.numChapters > 0) {
+            return false;
+        }
+
+        return true;
+    }
 
     cacheBooks = function (callback) {
         volumes.forEach(function (volume) {
@@ -81,6 +115,63 @@ const Scriptures = (function () {
             callback();
         }
     };
+
+    htmlAnchor = function (volume) {
+        return `<a name="${volume.id}" />`;
+    }
+    
+    htmlDiv = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let idString = "";
+    
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+    
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+    
+        if (parameters.id !== undefined) {
+            idString = ` id = "${parameters.id}"`;
+        }
+    
+        return `<div${idString}${classString}>${contentString}</div>`;
+    };
+    
+    htmlElement = function (tagName, content) {
+        return `<${tagName}>${content}</${tagName}>`;
+    }
+    
+    htmlLink = function (parameters) {
+        let classString = "";
+        let contentString = "";
+        let hrefString = "";
+        let idString = "";
+    
+        if (parameters.classKey !== undefined) {
+            classString = ` class="${parameters.classKey}"`;
+        }
+    
+        if (parameters.content !== undefined) {
+            contentString = parameters.content;
+        }
+    
+        if (parameters.href !== undefined) {
+            hrefString = ` href="${parameters.href}"`;
+        }
+    
+        if (parameters.id !== undefined) {
+            idString = ` id="${parameters.id}"`;
+        }
+    
+        return `<a${idString}${classString}${hrefString}>${contentString}</a>`;
+    };
+    
+    htmlHashLink = function (hashArguments, content) {
+        return `<a href="javascript:void(0)" onclick="changeHash(${hashArguments})">${content}</a>`;
+    }
 
     init = function (callback) {
         let booksLoaded = false;
@@ -156,12 +247,75 @@ const Scriptures = (function () {
         ]);
     };
 
+    navigateBook = function (bookId) {
+        console.log("navigateBook " + bookId);
+    };
+
+    navigateChapter = function (bookId, chapter) {
+        console.log("navigateChapter " + bookId + ", " + chapter);
+    };
+
+    navigateHome = function (volumeId) {
+        document.getElementById(DIV_SCRIPTURES).innerHTML =
+        "<div>Old Testament</div>" + 
+        "<div>New Testament</div>" + 
+        "<div>Book of Mormon</div>" + 
+        "<div>Doctrine and Covenants</div>" + 
+        "<div>Pearl of Great Price</div>" + volumeId;
+    };
+
+    onHashChanged = function () {
+        let ids = [];
+
+        // there is a location hash
+        if (location.hash !== "" && location.hash.length > 1) {
+            ids = location.hash.slice(1).split(":");
+        }
+        // length is less than 0
+        if (ids.length <= 0) {
+            navigateHome();
+        // length is 1
+        } else if (ids.length === 1) {
+            let volumeId = Number(ids[0]);
+
+            if (volumeId < volumes[0].id || volumeId > volumes.slice(-1)[0].id) {
+                // go home
+                navigateHome();
+            } else {
+                // go home with volume id
+                navigateHome(volumeId);
+            }
+        // length is greater than 2
+        } else {
+            let bookId = Number(ids[1]);
+
+            if (books[bookId] === undefined) {
+                navigateHome();
+            } else {
+                // go to the book
+                if (ids.length === 2) {
+                    navigateBook(bookId);
+                // go to the book chapter
+                } else {
+                    let chapter = Number(ids[2]);
+
+                    if (bookChapterValid(bookId, chapter)) {
+                        navigateChapter(bookId, chapter);
+                    } else {
+                        navigateHome();
+                    }
+                }
+            }
+        }
+    };
+
     /*-------------------------------------------------------------------
      *                      PUBLIC API
      */
     return {
         init,
-        testGeoplaces
+        testGeoplaces,
+        onHashChanged
     };
 
 }());
